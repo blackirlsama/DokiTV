4fe248a50bff5990a5c3a7208ee535c4a9056488
orange <799243133@qq.com>
2026-03-02 23:39:14 +0800

    over

--- src/main/java/com/orangecode1/tianmu/esdao/VideoEsDao.java
+++
@@ -1,9 +0,0 @@
-package com.orangecode.tianmu.esdao;
-
-import com.orangecode.tianmu.model.es.VideoEs;
-
-import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
-
-public interface VideoEsDao extends ElasticsearchRepository<VideoEs, Long> {
-
-}
--- src/main/java/com/orangecode1/tianmu/service/UserStatsService.java
+++
@@ -1,8 +0,0 @@
-package com.orangecode.tianmu.service;
-
-import com.baomidou.mybatisplus.extension.service.IService;
-import com.orangecode.tianmu.model.entity.UserStats;
-
-public interface UserStatsService extends IService<UserStats> {
-
-}
